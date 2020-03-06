a7c6e44c9a396568c7219209aa77469926d78a4b
wonderotter <gouk32@naver.com>
2020-03-06 09:13:41 +0900

    15. Annotations with Variables

--- features/variables.ts
+++ features/variables.ts
@@ -1,1 +1,9 @@
-const apples: number = 5;
+let apples: number = 5;
+let speed: string = '5';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//build in objects
+let now: Date = new Date();
