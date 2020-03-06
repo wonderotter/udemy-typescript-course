066c54644968b748c70e71c03f773c3db7435dc7
wonderotter <gouk32@naver.com>
2020-03-06 09:26:55 +0900

    16. Object Literal Annotations

--- features/variables.ts
+++ features/variables.ts
@@ -5,5 +5,21 @@ let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
-//build in objects
+// build in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, false, false, true];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
