0ea63f80b80bd7b9d34778b37f0d37ecf10fa29e
wonderotter <gouk32@naver.com>
2020-03-16 09:32:43 +0900

    25. Annotations for Anonymous Functions

--- functions.ts
+++ functions.ts
@@ -6,3 +6,11 @@ const add = (a: number, b: number): number => {
 const subtract = (a: number, b: number) => {
   a - b; //return을 쓰는 것을 잊었을 때 어떤 오류도 발생시키지 않는다.
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
