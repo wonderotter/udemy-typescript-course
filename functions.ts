9d79ea19eb475c963c20b1bfc09885d5c6b8021c
wonderotter <gouk32@naver.com>
2020-03-16 20:49:12 +0900

    26. Void and Never

--- functions.ts
+++ functions.ts
@@ -14,3 +14,24 @@ function divide(a: number, b: number): number {
 const multiply = function(a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+  // return null;
+  // return undefined
+};
+
+// never : never going to reach the end of this function
+// 절대 return 값이 없을 때만 사용한다,
+const throwError = (message: string): never => {
+  throw Error(message);
+};
+
+// return하는 경우가 있다면 never를 사용할 수 없다.
+const throwErro2 = (message: string): string => {
+  if (!message) {
+    throw Error(message);
+  }
+
+  return message;
+};
