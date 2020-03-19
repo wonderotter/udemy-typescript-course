fc80c8880a477a3db50ad518c4eee8a63a3cae95
wonderotter <gouk32@naver.com>
2020-03-19 21:54:28 +0900

    40. Functions in Interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,12 +1,12 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+interface Reportable {
+  //name: string;
+  //year: Date;
+  //broken: boolean;
   summary(): string;
 }
 
 const oldCivic = {
-  name: 'civic',
+  name: "civic",
   year: new Date(),
   broken: true,
   summary(): string {
@@ -14,8 +14,9 @@ const oldCivic = {
   }
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary());
+// summary property만 있어도 오류를 띄우지 않는다.
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
