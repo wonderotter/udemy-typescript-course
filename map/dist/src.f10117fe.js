b08bc05474d7657454da7083ab9c09ce26fa40fd
wonderotter <gouk32@naver.com>
2020-03-23 17:21:19 +0900

    52. Generating Random Data

--- map/dist/src.f10117fe.js
+++ map/dist/src.f10117fe.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60079" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61372" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
