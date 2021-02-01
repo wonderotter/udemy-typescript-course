eb0537ee8f36b8d29e5e31f9bd901163f2b5ba40
wonderotter <gouk32@naver.com>
2021-02-01 18:03:38 +0900

    130. Generating HTML Reports

--- stats/build/index.js
+++ stats/build/index.js
@@ -5,10 +5,13 @@ var CsvFileReader_1 = require("./CsvFileReader");
 var Summary_1 = require("./Summary");
 var WinsAnalysis_1 = require("./analyzer/WinsAnalysis");
 var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var HtmlReport_1 = require("./reportTargets/HtmlReport");
 // DataReader 인터페이스를 만족하는 객체 만들기
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // MatchReader의 인스턴스를 만들고 DataReader 인터페이스를 만족시키는 객체 넘기기
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+var summary2 = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
+summary2.buildAndPrintReport(matchReader.matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
 import { WinsAnalysis } from './analyzer/WinsAnalysis';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 // DataReader 인터페이스를 만족하는 객체 만들기
 const csvFileReader = new  CsvFileReader('football.csv');
@@ -13,5 +14,7 @@ matchReader.load();
 
 
 const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+const summary2 = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
+summary2.buildAndPrintReport(matchReader.matches);
