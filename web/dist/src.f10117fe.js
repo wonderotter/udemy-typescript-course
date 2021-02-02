089bb0dca8a71d95d371ce109ef0309e6394ee4c
wonderotter <gouk32@naver.com>
2021-02-02 16:11:11 +0900

    143. Optional Interface Properties

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -117,9 +117,52 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-console.log('hi there');
-},{}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.User = void 0;
+
+var User =
+/** @class */
+function () {
+  function User(data) {
+    this.data = data;
+  }
+
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  return User;
+}();
+
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./models/User");
+
+var user = new User_1.User({
+  name: 'myname',
+  age: 20
+});
+user.set({
+  name: "new_name"
+});
+console.log(user.get('name'));
+console.log(user.get('age'));
+},{"./models/User":"src/models/User.ts"}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there');
+import { User } from "./models/User";
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.set({ name: "new_name" });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
