e34ed6b4aa05e3b7b72d7ee0c55b27b35fc3d7f7
wonderotter <gouk32@naver.com>
2021-02-09 11:43:19 +0900

    190. Defining an Events Map

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -132,8 +132,18 @@ function () {
     this.parent = parent;
   }
 
+  UserForm.prototype.eventsMap = function () {
+    return {
+      'click:button': this.onButtonClick
+    };
+  };
+
+  UserForm.prototype.onButtonClick = function () {
+    alert('Hi there');
+  };
+
   UserForm.prototype.template = function () {
-    return "\n    <div>\n      <h1>User Form</h1>\n      <input type=\"text\"/>\n    </div>\n    ";
+    return "\n    <div>\n      <h1>User Form</h1>\n      <input type=\"text\"/><button>\uD074\uB9AD</button>\n    </div>\n    ";
   };
 
   UserForm.prototype.render = function () {
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,11 +1,21 @@
 export class UserForm {
   constructor(public parent: Element){}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick
+    };
+  }
+
+  onButtonClick(): void{
+    alert('Hi there');
+  }
+
   template(): string {
     return `
     <div>
       <h1>User Form</h1>
-      <input type="text"/>
+      <input type="text"/><button>클릭</button>
     </div>
     `
   }
