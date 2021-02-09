e69f669c0d3809f8c971212ed576cd8e9b5a8cd5
wonderotter <gouk32@naver.com>
2021-02-09 11:05:30 +0900

    185. A Class Method for Collections

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 export interface UserProps {
   id?: number;
@@ -18,6 +19,11 @@ export class User extends Model<UserProps>{
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
     );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps>{
+    return new Collection<User, UserProps>('http://localhost:3000/users', 
+    (json: UserProps) => User.buildUser(json));
   }
 }
 
