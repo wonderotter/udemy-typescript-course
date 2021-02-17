e636cf1cab8baf457f2609c268a5e952d896c0fa
wonderotter <gouk32@naver.com>
2021-02-17 10:20:39 +0900

    222. Protecting Routes

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -1,6 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send("\n  <h1>Not Permitted</h1>\n  <a href=\"/login\">Login<a/>\n  ");
+}
 var router = express_1.Router();
 exports.router = router;
 router.get("/login", function (req, res) {
@@ -10,7 +18,7 @@ router.post('/login', function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
     if (email && password && email === 'hi@hi.com' && password === 'password') {
         // mark this person as logged in
-        req.session = { loggedIn: true };
+        req.session = { loggedIn: true, username: email };
         //redirect them to the root route
         res.redirect('/');
     }
@@ -20,13 +28,23 @@ router.post('/login', function (req, res) {
 });
 router.get('/', function (req, res) {
     if (req.session && req.session.loggedIn) {
-        res.send("\n      <div>\n        <p>You are logged in</p>\n        <a href=\"/logout\">Logout</a>\n      </div>\n    ");
+        res.send("\n      <div>\n        <p>You are logged in</p>\n        <a href=\"/logout\">Logout</a>\n        <a href=\"/protected\">Mypage</a>\n      </div>\n    ");
     }
     else {
-        res.send("\n      <div>\n        <p>You are not logged in</p>\n        <a href=\"/login\">Login</a>\n      </div>\n    ");
+        res.send("\n      <div>\n        <p>You are not logged in</p>\n        <a href=\"/login\">Login</a>\n        <a href=\"/protected\">Mypage</a>\n      </div>\n    ");
     }
 });
 router.get('/logout', function (req, res) {
     req.session = undefined;
     res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    if (req.session && req.session.loggedIn && req.session.username) {
+        var username = req.session.username;
+        res.send("Welcome to " + username + " MyPage");
+    }
+    else {
+        res.status(500);
+        res.send('Server Error');
+    }
 });
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,10 +1,23 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 
 // Request의 기능을 모두 가져오고 잘못된 타입만 오버라이드한다.
 interface RequestWithBody extends Request{
   //override
   body: { [key:string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void{
+  if(req.session && req.session.loggedIn){
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send(`
+  <h1>Not Permitted</h1>
+  <a href="/login">Login<a/>
+  `);
 }
 
 const router = Router();
@@ -30,7 +43,7 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
 
   if(email && password && email === 'hi@hi.com' && password === 'password'){
     // mark this person as logged in
-    req.session = { loggedIn: true};
+    req.session = { loggedIn: true, username: email };
     //redirect them to the root route
     res.redirect('/');
   }else{
@@ -45,6 +58,7 @@ router.get('/', (req: Request, res: Response) => {
       <div>
         <p>You are logged in</p>
         <a href="/logout">Logout</a>
+        <a href="/protected">Mypage</a>
       </div>
     `);
   }else{
@@ -52,6 +66,7 @@ router.get('/', (req: Request, res: Response) => {
       <div>
         <p>You are not logged in</p>
         <a href="/login">Login</a>
+        <a href="/protected">Mypage</a>
       </div>
     `);
   }
@@ -60,6 +75,18 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: RequestWithBody, res: Response) => {
+ if(req.session && req.session.loggedIn && req.session.username){
+   const { username } = req.session;
+
+   res.send(`Welcome to ${username} MyPage`);
+ }else{
+   res.status(500);
+   res.send('Server Error');
+ }
+  
 });
 
 export { router };
